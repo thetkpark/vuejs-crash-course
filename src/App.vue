26370a4333d8635b3221da5e4eac66a6c939b3a9
Sethanant Pipatpakorn <sethanant.p@linecorp.com>
2022-06-01 10:40:08 +0700

    Finish UI

---
+++ src/App.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="container">
+    <Header
+      title="Task Tracker"
+      :showAddTask="showAddTask"
+      @toggle-add-task="toggleAddTask"
+    />
+    <AddTask v-if="showAddTask" @add-task="addTask" />
+    <Tasks
+      :tasks="tasks"
+      @delete-task="deleteTask"
+      @toggle-reminder="toggleReminder"
+    />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
+import AddTask from "./components/AddTask.vue";
+export default {
+  name: "App",
+  components: {
+    Header,
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    };
+  },
+  methods: {
+    deleteTask(id) {
+      if (confirm("Are you sure")) {
+        this.tasks = this.tasks.filter((t) => t.id !== id);
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((t) =>
+        t.id === id ? { ...t, reminder: !t.reminder } : t
+      );
+    },
+    addTask(newTask) {
+      this.tasks = [...this.tasks, newTask];
+    },
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctor Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting at School",
+        day: "March 3st at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Food Shopping",
+        day: "March 3st at 11:00am",
+        reminder: false,
+      },
+    ];
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    v-bind:style="{ background: color }"
+    v-on:click="onClick()"
+    class="btn"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit("btn-clicked");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
